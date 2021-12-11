f2fa992e995dc4a58a821f7e1afdb8dc28cab23a
Jan Schulte <jan@work-with-jan.com>
2021-12-11 15:24:45 -0500

    Add basic instrumentation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use tracing::{info, Level};
+use tracing_subscriber::FmtSubscriber;
+
+mod yak_shave;
+
 fn main() {
-    println!("Hello, world!");
+    // a builder for `FmtSubscriber`.
+    let subscriber = FmtSubscriber::builder()
+        // all spans/events with a level higher than TRACE (e.g, debug, info, warn, etc.)
+        // will be written to stdout.
+        .with_max_level(Level::TRACE)
+        // completes the builder.
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber)
+        .expect("setting default subscriber failed");
+
+    let number_of_yaks = 3;
+    // this creates a new event, outside of any spans.
+    info!(number_of_yaks, "preparing to shave yaks");
+
+    let number_shaved = yak_shave::shave_all(number_of_yaks);
+    info!(
+        all_yaks_shaved = number_shaved == number_of_yaks,
+        "yak shaving completed."
+    );
 }
---
+++ src/yak_shave.rs
@@ -0,0 +1,4 @@
+
+pub(crate) fn shave_all(number_of_yaks: i32) -> i32 {
+    number_of_yaks
+}

638c418281f3ad59196727cea9bd71c8df07209a
Jan Schulte <jan@work-with-jan.com>
2021-12-11 15:41:43 -0500

    Instrument library code

--- src/yak_shave.rs
+++ src/yak_shave.rs
@@ -1,4 +1,10 @@
+use tracing::info;
 
+#[tracing::instrument]
 pub(crate) fn shave_all(number_of_yaks: i32) -> i32 {
+    for yak_index in 0..number_of_yaks {
+        info!(current_yak=yak_index+1, "Shaving in progress");
+    }
+
     number_of_yaks
 }
